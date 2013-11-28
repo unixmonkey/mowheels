124eca3aaeeab16e387371b5b832a2df879b961a
Forrest Held <forrest.held@comcast.net>
2013-11-28 23:25:48 +0800

    Created joins between tables and input validation

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,12 @@
 class Recipient < ActiveRecord::Base
  has_many :recipient_menus, dependent: :destroy
  has_many :menus, through: :recipient_menus
+
+def first_last_name
+	
+  [first_name, last_name].reject{|v| v.blank?}.join(" ")
+
+end	
+
 end
+
